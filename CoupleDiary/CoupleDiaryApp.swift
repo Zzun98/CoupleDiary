a90b6e08f6087aa823ab9c44e176f58d8e28c22c
Zzun98 <sunnylucie98@gmail.com>
2023-10-13 09:54:18 +1100

    Initial Commit

---
+++ CoupleDiary/CoupleDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoupleDiaryApp.swift
+//  CoupleDiary
+//
+//  Created by Sunjun Kwak on 13/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoupleDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
