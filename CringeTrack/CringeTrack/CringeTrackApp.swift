c2a912672f4ce94c1a1be5557da6da15b9cd33ec
Christopher Av <choclate00@live.com>
2023-10-22 16:41:23 +1100

    Added CoreData Persistence Controller.

--- CringeTrack/CringeTrack/CringeTrackApp.swift
+++ CringeTrack/CringeTrack/CringeTrackApp.swift
@@ -6,11 +6,14 @@
 //
 
 import SwiftUI
+import CoreData
 
 @main struct CringeTrackApp: App {
+    let persistenceController = PersistenceController.shared
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
