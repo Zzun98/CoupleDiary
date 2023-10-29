ba2db89e7379c7b4940e65a7cc54f30027007c14
Christopher Av <choclate00@live.com>
2023-10-29 15:06:23 +1100

    Did some backend stuff to handle selected photos from the photo picker for Albums, fixed a bug where the albumData did not fetch from CoreData.

--- CringeTrack/CringeTrack/ViewModel/AlbumViewModel.swift
+++ CringeTrack/CringeTrack/ViewModel/AlbumViewModel.swift
@@ -12,7 +12,17 @@ import PhotosUI
 
 class AlbumViewModel: ObservableObject {
     @Published var albumnData = [CoupleMemoryStruct]()
-    
+    @Published var selectedImage: PhotosPickerItem? = nil {
+        didSet {
+            do {
+                try setImageRawData(imageFromPhotoPicker: selectedImage!)
+            } catch {
+                print("Failed converting to raw data.")
+            }
+        }
+    }
+    //this is a variable that will have the image data from the photopickeritem.
+    @Published var selectedImageBinary: Data?
     //this function will process uploading the image from the image picker to CoreData
     func saveImage(coupleMemory: CoupleMemoryStruct) {
         do {
@@ -25,9 +35,20 @@ class AlbumViewModel: ObservableObject {
     func loadAlbumItems() {
         do {
             self.albumnData = try CoreDataManager.loadAlbumns()
+            print("Loaded albumn data.")
         } catch {
             print("Failed to load albumns.")
+            print(error)
+            print(error.localizedDescription)
         }
         
+    }
+    
+    func setImageRawData(imageFromPhotoPicker: PhotosPickerItem) throws {
+        Task {
+            if let data = try await imageFromPhotoPicker.loadTransferable(type: Data.self) {
+                self.selectedImageBinary = data
+            }
+        }
     }
 }
