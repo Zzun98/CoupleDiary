7f6ce94521bcb217a96fea3d2d7d8635197f0886
Denni <DennieEom2023@gmail.com>
2023-11-01 13:18:45 +1100

    fixed syntax errors

--- CringeTrack/CringeTrack/Model/MonthStruct.swift
+++ CringeTrack/CringeTrack/Model/MonthStruct.swift
@@ -10,16 +10,19 @@
 import Foundation
 import SwiftUI
 
-struct MonthStruct {
-    var monthType: MonthType
-    var dayInt : Int
-    func day() -> String {
-        return String(dayInt)
+extension Date {
+    func daysInMonth() -> Int {
+        let calendar = Calendar.current
+        let range = calendar.range(of: .day, in: .month, for: self)!
+        return range.count
     }
-}
-
-enum MonthType {
-    case Previous
-    case Current
-    case Next
+    
+    func firstDayOfMonth() -> Int {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year, .month, .day], from: self)
+        let firstDay = calendar.date(from: DateComponents(year: components.year, month: components.month, day: 1))!
+        return calendar.component(.weekday, from: firstDay)
+    }
+    
+   
 }
